7049e3c133684c2ecfbcf9a5dca409e6b1f6e5d7
= <=>
2024-08-19 11:18:53 +0700

    "home page tournament section"

---
+++ app/ui/home/tournament-list.tsx
@@ -0,0 +1,145 @@
+import {
+  faCalendar,
+  faEarthAsia,
+  faTrophy,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+import Link from "next/link";
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+export default function TournamentList() {
+  const slideData = [
+    {
+      id: 1,
+      name: "Badminton 1",
+      img: "/image/carousel/Badminton.webp",
+      imgAlt: "badminton",
+      owner: "tounament owner",
+      format: "single elimination",
+      participantNumber: "6",
+      createAt: "created 8/15/2024",
+    },
+    {
+      id: 2,
+      name: "Badminton 2",
+      img: "/image/carousel/Badminton.webp",
+      imgAlt: "badminton",
+      owner: "tounament owner",
+      format: "double elimination",
+      participantNumber: "6",
+      createAt: "created 8/15/2024",
+    },
+    {
+      id: 3,
+      name: "Badminton 3",
+      img: "/image/carousel/Badminton.webp",
+      imgAlt: "badminton",
+      owner: "tounament owner",
+      format: "single elimination",
+      participantNumber: "6",
+      createAt: "created 8/15/2024",
+    },
+    {
+      id: 4,
+      name: "Badminton 4",
+      img: "/image/carousel/Badminton.webp",
+      imgAlt: "badminton",
+      owner: "tounament owner",
+      format: "double elimination",
+      participantNumber: "6",
+      createAt: "created 8/15/2024",
+    },
+    {
+      id: 5,
+      name: "Badminton 5",
+      img: "/image/carousel/Badminton.webp",
+      imgAlt: "badminton",
+      owner: "tounament owner",
+      format: "single elimination",
+      participantNumber: "6",
+      createAt: "created 8/15/2024",
+    },
+    {
+      id: 6,
+      name: "Badminton 6",
+      img: "/image/carousel/Badminton.webp",
+      imgAlt: "badminton",
+      owner: "tounament owner",
+      format: "double elimination",
+      participantNumber: "6",
+      createAt: "created 8/15/2024",
+    },
+  ];
+  return (
+    <div className="w-full overflow-hidden">
+      <Swiper
+        className="flex items-center gap-7 flex-row"
+        spaceBetween={30}
+        slidesPerView={3}
+        navigation={{
+          nextEl: ".custom-swiper-button-next",
+          prevEl: ".custom-swiper-button-prev",
+        }}
+        modules={[Navigation]}
+        breakpoints={{
+          1024: { slidesPerView: 3 },
+          768: { slidesPerView: 2.5 },
+          640: { slidesPerView: 2 },
+          320: { slidesPerView: 1 },
+        }}
+      >
+        {slideData.map((item) => (
+          <SwiperSlide key={item.id}>
+            <Link href={"/"} className="min-w-full md:min-w-[340px]">
+              <div className="h-full p-1 border-transparent hover:rounded-2xl hover:bg-[#ef8e5e] active:bg-[#ef8e5e]">
+                <div className="flex flex-col gap-0 h-[calc(100%-8px)] rounded-2xl border border-black bg-[#191919] text-lg overflow-hidden">
+                  <Image
+                    src={item.img}
+                    width={300}
+                    height={150}
+                    alt="..."
+                    style={{
+                      width: "100%",
+                      height: "150px",
+                      maxWidth: "100%",
+                    }}
+                  ></Image>
+                  <div className="flex flex-col gap-8 p-7">
+                    <div className="flex flex-col gap-5 text-white">
+                      <p className="text-xl font-bold mb-3">{item.name}</p>
+                      <div className="flex flex-row gap-4 items-center">
+                        <FontAwesomeIcon
+                          className="size-5"
+                          icon={faEarthAsia}
+                        />
+                        <p className="text-base">{item.owner}</p>
+                      </div>
+                      <div className="flex flex-row gap-4 items-center">
+                        <FontAwesomeIcon className="size-5" icon={faTrophy} />
+                        <p className="text-base">{item.format}</p>
+                      </div>
+                      <div className="flex flex-row gap-4 items-center">
+                        <FontAwesomeIcon className="size-5" icon={faUser} />
+                        <p className="text-base">{item.participantNumber}</p>
+                      </div>
+                      <div className="flex flex-row gap-4 items-center">
+                        <FontAwesomeIcon className="size-5" icon={faCalendar} />
+                        <p className="text-base">{item.createAt}</p>
+                      </div>
+                    </div>
+                    <button className="text-lg text-white font-normal w-full h-12 min-w-12 pe-5 ps-5 gap-2 bg-gradient-to-b from-[#606060] to-[#484848] hover:bg-gradient-to-b hover:from-[#484848] hover:to-[#3a3a3a]">
+                      VIEW TOURNAMENT
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
---
+++ app/ui/home/tournament.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import {
+  faArrowRight,
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+import Link from "next/link";
+import { EffectCoverflow, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import TournamentList from "./tournament-list";
+
+export default function Tournament() {
+  
+  return (
+    <div className="flex flex-col xl:flex-row gap-20 bg-gradient-to-tr from-[#3a3a3a] to-[#606060] -mt-20 px-6 pt-44 pb-20 lg:pl-32 lg:pr-6 border-t-[15px] border-t-[#484848] w-full max-w-[2200px]">
+      <div className="flex flex-col gap-7 max-w-full lg:max-w-[600px] text-white">
+        <h2 className="text-xl md:text-2xl italic font-bold">
+          JOIN THE MASSES
+        </h2>
+        <h2 className="text-3xl md:text-5xl italic font-bold leading-[2.375rem] md:leading-[3.75rem]">
+          Manage Your Next Tournament With Challonge
+        </h2>
+        <p className="text-base md:text-lg leading-8 text-[#b4b4b4]">
+          For almost 15 years Challonge has been offering a web-based SaaS
+          platform, as well as an API that can be integrated into websites,
+          games, bots, and other software applications. Everything about the
+          user experience has been developed with you in mind. Simple, quick,
+          and reliable. That’s the Challonge difference.
+        </p>
+        <div className="flex flex-col md:flex-row gap-3">
+          <div>
+            <Link key={"create-tour"} href={"/"}>
+              <button className="text-white bg-[#f8844a] hover:bg-[#f3752c] w-full font-bold rounded-full italic px-10 py-2.5 text-center">
+                CREATE A TOURNAMENT
+              </button>
+            </Link>
+          </div>
+          <div>
+            <Link key={"search-tour"} href={"/"}>
+              <button className="text-white hover:bg-[#555] font-bold rounded-full italic px-5 py-2.5 text-center justify-center flex w-full gap-3">
+                <span className="self-center">SEARCH TOURNAMENTS</span>
+                <FontAwesomeIcon
+                  icon={faArrowRight}
+                  className="size-5 self-center"
+                />
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="bg-right-top bg-[url(https://assets.challonge.com/_next/static/media/reverse-bracket-bg.4c49a3cc.png)] bg-cover bg-no-repeat overflow-auto">
+        <div className="text-[#b4b4b4] flex flex-col-reverse md:flex-row items-start md:items-center gap-5 mb-10">
+          <div>
+            <button className="custom-swiper-button-prev h-10 min-w-10 text-sm pe-0 ps-0 gap-2">
+              <FontAwesomeIcon className="w-5 h-5" icon={faChevronLeft} />
+            </button>
+            <button className="custom-swiper-button-next h-10 min-w-10 text-sm pe-0 ps-0 gap-2">
+              <FontAwesomeIcon className="w-5 h-5" icon={faChevronRight} />
+            </button>
+          </div>
+          <h2 className="text-xl md:text-2xl font-bold italic">
+            36M+ Brackets Created
+          </h2>
+        </div>
+        <TournamentList/>
+      </div>
+    </div>
+  );
+}
