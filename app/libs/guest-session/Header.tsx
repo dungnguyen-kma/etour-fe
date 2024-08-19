93692b1ea2e55d339526f385d0f9abc586786ae0
= <=>
2024-08-19 17:38:25 +0700

    "fix overflow header"

--- app/libs/guest-session/Header.tsx
+++ app/libs/guest-session/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function GuestHeader() {
   return (
-    <div className="bg-[#484848] shadow-md h-14 lg:h-16 flex flex-row fixed top-0 w-full justify-between lg:py-3.5 md:py-3 py-3 z-20">
+    <div className="bg-[#484848] shadow-md h-14 lg:h-16 flex flex-row fixed top-0 w-full max-w-[100vw] justify-between lg:py-3.5 py-3 z-20">
       <div className="min-w-[62px] max-w-80 md:w-80 lg:pl-12 lg:pr-4 md:pl-4 md:pr-20 pl-4">
         <Link key={"home"} href="/" className="">
           <div className="md:bg-[url(https://assets.challonge.com/_next/static/media/logo-white-text.928c8884.svg)] bg-contain bg-no-repeat h-[40px] bg-[url(https://assets.challonge.com/_next/static/media/logo-symbol-only.8b0dbfc7.svg)]"></div>
@@ -13,7 +13,7 @@ export default function GuestHeader() {
       <div className="flex gap-6 lg:pr-12 md:pr-4 pr-4 items-center">
         <div>
           <Link key={"search"} href={"/search"}>
-            <button className="text-white w-5 h-5 pt-1 opacity-70 hover:opacity-100">
+            <button className="text-white w-5 h-5 opacity-70 hover:opacity-100">
               <FontAwesomeIcon icon={faMagnifyingGlass} />
             </button>
           </Link>
