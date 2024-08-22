a354f8b499cafba8b6963430290fedd41fa31970
= <=>
2024-08-22 10:31:36 +0700

    add side nav mobile page

---
+++ app/libs/Header.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import Link from "next/link";
+import NavLinks from "./nav-links";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { AnimatePresence, motion } from "framer-motion";
+import { Hidden } from "@mui/material";
+import { exit } from "process";
+
+export default function GuestHeader() {
+  const [isNavVisible, setIsNavVisible] = useState(false);
+
+  const handleClickLink = () => {
+    setIsNavVisible(false);
+  };
+
+  return (
+    <div className="bg-[#484848] shadow-md h-14 lg:h-16 flex flex-row fixed top-0 w-full justify-between lg:py-3.5 py-3 z-20">
+      <div className="min-w-[62px] max-w-80 md:w-80 lg:pl-12 lg:pr-4 md:pr-20 pl-4">
+        <Link href="/" className="">
+          <div className="md:bg-[url(https://assets.challonge.com/_next/static/media/logo-white-text.928c8884.svg)] bg-contain bg-no-repeat h-[40px] bg-[url(https://assets.challonge.com/_next/static/media/logo-symbol-only.8b0dbfc7.svg)]"></div>
+        </Link>
+      </div>
+      <div
+        className="px-4 text-white my-auto flex items-center"
+        onClick={() => setIsNavVisible(!isNavVisible)}
+      >
+        <FontAwesomeIcon icon={faBars} className="size-6" />
+      </div>
+      <AnimatePresence>
+        {isNavVisible && (
+          <motion.div
+            className="absolute top-14 right-0 flex flex-col items-end bg-black w-screen h-screen bg-opacity-40"
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            variants={{
+              hidden: { opacity: 0 },
+              visible: { opacity: 1 },
+              exit: { opacity: 0 },
+            }}
+            transition={{
+              duration: 0.5,
+              ease: "easeInOut",
+            }}
+          >
+            <motion.div
+              className="w-fit h-screen bg-[#484848] flex flex-col items-end gap-6 text-white italic font-bold text-sm px-4"
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              variants={{
+                hidden: { x: 100 },
+                visible: { x: 0 },
+                exit: { x: 100 },
+              }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+            >
+              <NavLinks onClickLink={handleClickLink} />
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+}
---
+++ app/libs/nav-links.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+
+export default function NavLinks({ onClickLink }: any) {
+  const pathname = usePathname();
+  const links = [
+    { id: 1, name: "HOME", href: "/" },
+    { id: 2, name: "TOURNAMENT", href: "/tournament" },
+    { id: 3, name: "COMMUNITY", href: "/community" },
+    { id: 4, name: "BRACKET GENERATOR", href: "/bracket-generator" },
+    { id: 5, name: "SIGNIN/SIGNUP", href: "/signin" },
+  ];
+  return (
+    <>
+      {links.map((link) => (
+        <Link
+          key={link.id}
+          href={link.href}
+          className={clsx(
+            "relative before:content-[''] before:absolute before:top-full before:w-0 before:h-0.5 before:left-0 before:bg-[#f8844a] hover:before:w-full before:duration-300",
+            { "text-[#f8844a] before:w-full": pathname === link.href }
+          )}
+          onClick={onClickLink}
+        >
+          <p>{link.name}</p>
+        </Link>
+      ))}
+    </>
+  );
+}
