325c465030efdaa9708bbbacc89548d4d053831b
= <=>
2024-08-22 22:36:13 +0700

    responsive NavLink

--- app/libs/Header.tsx
+++ app/libs/Header.tsx
@@ -2,19 +2,24 @@
 
 import Link from "next/link";
 import NavLinks from "./nav-links";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { AnimatePresence, motion } from "framer-motion";
-import { Hidden } from "@mui/material";
-import { exit } from "process";
+import { useBreakPoint } from "../hooks/useBreakPoint";
 
-export default function GuestHeader() {
+export default function Header() {
   const [isNavVisible, setIsNavVisible] = useState(false);
+  const { sm, md, lg, xl, xxl } = useBreakPoint();
 
-  const handleClickLink = () => {
-    setIsNavVisible(false);
-  };
+  useEffect(() => {
+    console.log(lg);
+    if (lg) {
+      setIsNavVisible(true);
+    } else {
+      setIsNavVisible(false);
+    }
+  }, [lg]);
 
   return (
     <div className="bg-[#484848] shadow-md h-14 lg:h-16 flex flex-row fixed top-0 w-full justify-between lg:py-3.5 py-3 z-20">
@@ -23,19 +28,21 @@ export default function GuestHeader() {
           <div className="md:bg-[url(https://assets.challonge.com/_next/static/media/logo-white-text.928c8884.svg)] bg-contain bg-no-repeat h-[40px] bg-[url(https://assets.challonge.com/_next/static/media/logo-symbol-only.8b0dbfc7.svg)]"></div>
         </Link>
       </div>
-      <div
-        className="px-4 text-white my-auto flex items-center"
-        onClick={() => setIsNavVisible(!isNavVisible)}
-      >
-        <FontAwesomeIcon icon={faBars} className="size-6" />
-      </div>
+      {!lg && (
+        <div
+          className="px-4 text-white my-auto flex items-center"
+          onClick={() => setIsNavVisible(!isNavVisible)}
+        >
+          <FontAwesomeIcon icon={faBars} className="size-6" />
+        </div>
+      )}
       <AnimatePresence>
         {isNavVisible && (
           <motion.div
-            className="absolute top-14 right-0 flex flex-col items-end bg-black w-screen h-screen bg-opacity-40"
-            initial="hidden"
-            animate="visible"
-            exit="exit"
+            className="absolute top-14 right-0 flex flex-col items-end bg-black w-screen h-screen bg-opacity-40 lg:relative lg:w-auto lg:h-auto lg:flex-row lg:top-0 lg:items-center lg:bg-transparent lg:pr-12"
+            initial={lg ? "null" : "hidden"}
+            animate={lg ? "null" : "visible"}
+            exit={lg ? "null" : "exit"}
             variants={{
               hidden: { opacity: 0 },
               visible: { opacity: 1 },
@@ -45,13 +52,13 @@ export default function GuestHeader() {
               duration: 0.5,
               ease: "easeInOut",
             }}
-            onClick={() => setIsNavVisible(false)}
+            onClick={() => !lg && setIsNavVisible(false)}
           >
             <motion.div
-              className="w-fit h-screen bg-[#484848] flex flex-col items-end gap-6 text-white italic font-bold text-sm px-4"
-              initial="hidden"
-              animate="visible"
-              exit="exit"
+              className="w-fit h-screen bg-[#484848] flex flex-col items-end gap-6 text-white italic font-bold text-sm px-4 lg:px-0 lg:text-base lg:flex-row lg:items-center lg:h-auto"
+              initial={lg ? "null" : "hidden"}
+              animate={lg ? "null" : "visible"}
+              exit={lg ? "null" : "exit"}
               variants={{
                 hidden: { x: 100 },
                 visible: { x: 0 },
@@ -59,7 +66,7 @@ export default function GuestHeader() {
               }}
               transition={{ duration: 0.5, ease: "easeInOut" }}
             >
-              <NavLinks onClickLink={handleClickLink} />
+              <NavLinks onClickLink={() => !lg && setIsNavVisible(false)} />
             </motion.div>
           </motion.div>
         )}
